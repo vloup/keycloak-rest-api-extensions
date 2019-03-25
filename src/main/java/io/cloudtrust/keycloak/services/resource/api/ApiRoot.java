4321d4c96daba37114f1c465914ecc083b49f9ba
Vincent Loup <vincent.loup@elca.ch>
2019-03-25 17:30:42 +0100

    Fix possible NPE exception.

--- src/main/java/io/cloudtrust/keycloak/services/resource/api/ApiRoot.java
+++ src/main/java/io/cloudtrust/keycloak/services/resource/api/ApiRoot.java
@@ -3,17 +3,9 @@ package io.cloudtrust.keycloak.services.resource.api;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpResponse;
-import org.jboss.resteasy.spi.NotFoundException;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.resteasy.spi.UnauthorizedException;
-import org.keycloak.jose.jws.JWSInput;
-import org.keycloak.jose.jws.JWSInputException;
-import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.representations.AccessToken;
-import org.keycloak.services.managers.AuthenticationManager;
-import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.resources.Cors;
 import org.keycloak.services.resources.admin.AdminAuth;
 import org.keycloak.services.resources.admin.AdminCorsPreflightService;
@@ -23,7 +15,6 @@ import org.keycloak.services.resources.admin.RealmsAdminResource;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 
 public class ApiRoot extends AdminRoot {
 
@@ -48,12 +39,14 @@ public class ApiRoot extends AdminRoot {
         AdminAuth auth = authenticateRealmAdminRequest(request.getHttpHeaders());
         if (auth != null) {
             logger.debug("authenticated admin access for: " + auth.getUser().getUsername());
+
+            Cors.add(request).allowedOrigins(auth.getToken()).allowedMethods("GET", "PUT", "POST", "DELETE").exposedHeaders("Location").auth().build(response);
+
+            RealmsAdminResource adminResource = new RealmsApiResource(auth, tokenManager, session);
+            ResteasyProviderFactory.getInstance().injectProperties(adminResource);
+            return adminResource;
+        } else {
+            throw new UnauthorizedException("Unauthenticated admin");
         }
-
-        Cors.add(request).allowedOrigins(auth.getToken()).allowedMethods("GET", "PUT", "POST", "DELETE").exposedHeaders("Location").auth().build(response);
-
-        RealmsAdminResource adminResource = new RealmsApiResource(auth, tokenManager, session);
-        ResteasyProviderFactory.getInstance().injectProperties(adminResource);
-        return adminResource;
     }
 }
